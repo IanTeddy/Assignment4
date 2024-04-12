824e5975c9d634e2c21e42db6872cd28ad0aea96
MegumiKabasawa <me398868730@gmail.com>
2024-04-12 14:29:56 -0400

    add detail page

--- script.js
+++ script.js
@@ -1,112 +1,45 @@
-const queryInput = document.querySelector(".query-input");
-const searchButton = document.querySelector(".search-btn");
 
 const API_KEY = "HcXLlHB3JAI1U4BBrhUsC4Mu2Zl9Uhdt1YHYeGj0";
 
-// retrive the Astronomy Picture of the Day
+const queryInput = document.querySelector(".query-input");
+const searchButton = document.getElementById('searchButton');
+
 const getAstroData = () => {
     const inputDate = queryInput.value;
-
-    // check if input area is empty or not
     if (inputDate === "") return;
+
     const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${inputDate}`;
     //https://api.nasa.gov/planetary/apod?api_key=HcXLlHB3JAI1U4BBrhUsC4Mu2Zl9Uhdt1YHYeGj0&date=2011-09-14
-
-    // fetching API Data, send a GET request and response is then converted to JSON format
-    fetch(API_URL).then(response => response.json()).then(data => {
-        // if data is empty 
+   
+    fetch(API_URL)
+    .then(response => response.json())
+    .then(data => {
         if (!data) return alert(`Request date might be out of date or invalid input`);
-        // destructuring assignment from API response
-        const { date, explanation, title, url } = data;
-        // call getAstroDetails function
-        //getAstroDetails(date, explanation, title, url);
-
-        console.log("Date:", date);
-        console.log("Explanation:", explanation);
-        console.log("Title:", title);
-        console.log("URL:", url);
-
+        localStorage.setItem('data', JSON.stringify(data));
+        window.location.href = "detail.html";
+        
     }).catch(() => {
         alert("An error occurred while fetching the data");
     });
+}
+document.addEventListener('DOMContentLoaded', function() {
+    searchButton.addEventListener("click", getAstroData);
+});
 
-    //clear input data and previous data
+
+function displayStoredData() {
+    const savedData = localStorage.getItem('data');
+    const parsedData = JSON.parse(savedData);
     
+    const dateElement = document.getElementById('date');
+    const explanationElement = document.getElementById('explanation');
+    const titleElement = document.getElementById('title');
+    const urlElement = document.getElementById('url');
+    
+    dateElement.textContent = parsedData.date;
+    explanationElement.textContent = parsedData.explanation;
+    titleElement.textContent = parsedData.title;
+    urlElement.innerHTML = `<a href="${parsedData.url}">${parsedData.url}</a>`;
 }
 
-/*
-
-    Blow is still from Weather API (I only changed above the block)
-
-*/
-
-// const locationButton = document.querySelector(".location-btn");
-// const currentWeatherDiv = document.querySelector(".current-weather");
-// const weatherCardsDiv = document.querySelector(".weather-cards");
-
-
-
-
-// const createAstroPic = (cityName, weatherItem, index) => {
-//     if(index === 0) { // HTML for the main weather card
-//         return `<div class="details">
-//                     <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
-//                     <h6>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
-//                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-//                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
-//                 </div>
-//                 <div class="icon">
-//                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-//                     <h6>${weatherItem.weather[0].description}</h6>
-//                 </div>`;
-//     } else { // HTML for the other five day forecast card
-//         return `<li class="card">
-//                     <h3>(${weatherItem.dt_txt.split(" ")[0]})</h3>
-//                     <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
-//                     <h6>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h6>
-//                     <h6>Wind: ${weatherItem.wind.speed} M/S</h6>
-//                     <h6>Humidity: ${weatherItem.main.humidity}%</h6>
-//                 </li>`;
-//     }
-// }
-
-// const getAstroDetails = (cityName, latitude, longitude) => {
-//     const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
-
-//     fetch(WEATHER_API_URL).then(response => response.json()).then(data => {
-//         // Filter the forecasts to get only one forecast per day
-//         const uniqueForecastDays = [];
-//         const fiveDaysForecast = data.list.filter(forecast => {
-//             const forecastDate = new Date(forecast.dt_txt).getDate();
-//             if (!uniqueForecastDays.includes(forecastDate)) {
-//                 return uniqueForecastDays.push(forecastDate);
-//             }
-//         });
-
-//         // Clearing previous weather data
-//         cityInput.value = "";
-//         currentWeatherDiv.innerHTML = "";
-//         weatherCardsDiv.innerHTML = "";
-
-//         // Creating weather cards and adding them to the DOM
-//         fiveDaysForecast.forEach((weatherItem, index) => {
-//             const html = createWeatherCard(cityName, weatherItem, index);
-//             if (index === 0) {
-//                 currentWeatherDiv.insertAdjacentHTML("beforeend", html);
-//             } else {
-//                 weatherCardsDiv.insertAdjacentHTML("beforeend", html);
-//             }
-//         });        
-//     }).catch(() => {
-//         alert("An error occurred while fetching the weather forecast!");
-//     });
-// }
-
-
-
-searchButton.addEventListener("click", getAstroData);
-// queryInput.addEventListener("keyup", e => e.key === "Enter" && getCityCoordinates());
-
-
-
-// locationButton.addEventListener("click", getUserCoordinates);
+window.addEventListener('load', displayStoredData);
